606b8fabb4fc6e354a52d5bb26173d8a87f06805
anastasia-anisichkina <nastya_pavlova19@list.ru>
2022-01-29 15:12:37 +0300

    проходимся по массиву считываем данные и выполняем нужную операцию со строками саммива

--- control/Program.cs
+++ control/Program.cs
@@ -1,2 +1,7 @@
 ﻿// создание и заполнение массива
 string[] daysOfWeek = { "fjgf", "sdg", "q","sdgsdgsdzvzvc", "Tdg", "we", "gs1" };
+
+// вычисляем строки меньше или равные 3 
+for (int i = 0; i < daysOfWeek.Length-1; i++){
+
+ if(daysOfWeek[i].Length<=3)
